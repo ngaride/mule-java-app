48ef4b4ba1721fe1db7227e368920b7fdd6baadb
ngaride <ngaride@gmail.com>
2020-04-05 20:46:11 -0400

    code commit

---
+++ src/main/java/com/abc/util/Stringmanipulate.java
@@ -0,0 +1,31 @@
+package com.abc.util;
+
+public class Stringmanipulate {
+	
+	public static String  ToCamel(String inputString)
+	{
+		// Extract all words
+		 		String words[] = inputString.split("\\s+");
+		 		
+		 		// Creating an empty string of type StringBuilder so that modification of string is possible.
+		 		StringBuilder sb = new StringBuilder();
+		 		
+		 		// Iterating through words
+		 		for (String word : words) {
+		 			//Extracting first char
+		 			char firstChar = word.charAt(0);
+		 			// Checking if firstchar is not in upper case already
+		 			if (!Character.isUpperCase(firstChar)) {
+		 				// Convert first char into upper case and then append remaining characters of words. 
+		 				sb.append(Character.toUpperCase(firstChar)).append(word.substring(1));
+		 			} else
+		 				sb.append(word.substring(0));
+		 			
+		 		}
+		 		
+		 		// Converting StringBuilder to String. trim() is needed to trim last space appended. 
+		 		String result = sb.toString().trim();
+		 		return result;
+	}
+
+}
